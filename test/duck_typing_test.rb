a616603a87988d5858f60525c8a3e688771a808d
nimid <ss.saroj@gmail.com>
2018-01-27 20:18:07 +0700

    First commit

---
+++ test/duck_typing_test.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+
+class A
+  def method
+    'Class A'
+  end
+end
+
+class B
+  def method
+    'Class B'
+  end
+end
+
+class C
+  def different_method_name
+    'Class C'
+  end
+end
+
+def test(object)
+  object.method
+end
+
+class DuckTypingTest < Test::Unit::TestCase
+  def test_duck_typing
+    a = A.new
+    b = B.new
+    c = C.new
+
+    assert_equal('Class A', test(A.new))
+    assert_equal('Class B', test(B.new))
+    assert_raise ArgumentError do
+      test(C.new)
+    end
+  end
+end
